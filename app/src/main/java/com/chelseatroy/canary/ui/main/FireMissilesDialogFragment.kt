0faa6fa01714b51cf681b8581f2f1bdeace16fc3
Chelsea Troy <chelsea.dommert@gmail.com>
2020-05-14 23:21:00 -0500

    Update the theme to something greener.

---
+++ app/src/main/java/com/chelseatroy/canary/ui/main/FireMissilesDialogFragment.kt
@@ -0,0 +1,31 @@
+package com.chelseatroy.canary.ui.main
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import com.chelseatroy.canary.R
+
+
+class FireMissilesDialogFragment : DialogFragment() {
+
+//    override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+//        return activity?.let {
+//            // Use the Builder class for convenient dialog construction
+//            val builder = AlertDialog.Builder(it)
+//            builder.setMessage("Fire missiles?")
+//                .setPositiveButton("FIRE!",
+//                    DialogInterface.OnClickListener { dialog, id ->
+//                        // FIRE ZE MISSILES!
+//                    })
+//                .setNegativeButton(
+//                    "CANCEL",
+//                    DialogInterface.OnClickListener { dialog, id ->
+//                        // User cancelled the dialog
+//                    })
+//            // Create the AlertDialog object and return it
+//            builder.create()
+//        } ?: throw IllegalStateException("Activity cannot be null")
+//    }
+}
