515c296f8f4fea43286f4c5670c6d534c91bbc9e
Chelsea Troy <chelsea.dommert@gmail.com>
2020-05-18 11:57:26 -0500

    Update tests for MoodEntry and HistoryFragment to reflect their current functionality
    + Mood Entry needed some helper methods to move between display format and database format

--- app/src/test/java/com/chelseatroy/canary/data/MoodEntryTest.kt
+++ app/src/test/java/com/chelseatroy/canary/data/MoodEntryTest.kt
@@ -5,12 +5,40 @@ import org.junit.Test
 
 class MoodEntryTest {
     @Test
-    fun toString_stringifiesMoodEntry() {
-        val moodEntry = MoodEntry(Mood.ELATED)
-        moodEntry.recentPastimes = arrayListOf(Pastime.EATING, Pastime.READING, Pastime.SLEEP)
-        moodEntry.notes = "Had a decent day!"
+    fun getFormattedLogTime_putsMoodEntryTimeInLegibleFormat() {
+        assertEquals("Monday, 18 May, 2020, 10:36 AM",
+            MoodEntry.getFormattedLogTime(1589816172396))
+    }
 
-        assertEquals("Mood Entry(mood: ELATED, notes: Had a decent day!, pastimes: [EATING, READING, SLEEP]",
+    @Test
+    fun formatForDatabase_putsMoodEntryPastimesInString_withEmptyDefault() {
+        assertEquals("READING, SLEEP",
+            MoodEntry.formatForDatabase(arrayListOf(Pastime.READING, Pastime.SLEEP)))
+        assertEquals("",
+            MoodEntry.formatForDatabase(ArrayList()))
+
+    }
+
+    @Test
+    fun formatForView_putsMoodEntryPastimesInString_withADefault() {
+        assertEquals("READING, SLEEP",
+            MoodEntry.formatForView(arrayListOf(Pastime.READING, Pastime.SLEEP)))
+        assertEquals("None :(",
+            MoodEntry.formatForView(ArrayList()))
+    }
+
+    @Test
+    fun toString_stringifiesMoodEntry() {
+        val moodEntry = MoodEntry(
+            Mood.ELATED,
+            1589816172396,
+            "Had a decent day!",
+            "EATING, READING, SLEEP"
+        )
+
+        assertEquals("Mood Entry(loggedAt: Monday, 18 May, 2020, 10:36 AM, mood: ELATED, notes: Had a decent day!, pastimes: [EATING, READING, SLEEP]",
         moodEntry.toString())
     }
+
+
 }
